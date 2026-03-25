3ce4a646189844b91afa6af607da5bece501570c
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-25 18:39:31 +0100

    i am testing it with the German Word Staubsauger to see if promting makes a differance - i does

--- comaringIpaWords.py
+++ comaringIpaWords.py
@@ -2,14 +2,16 @@ import html
 import unicodedata
 
 # source IPA (HTML-escaped) for component extraction
-source_escape_compIPA = "zɔ.nənˌbʏ.mə"
+#source_escape_compIPA = "zɔ.nənˌbʏ.mə"
+source_escape_compIPA = "ˈʃtaʊ̯pˌzaʊ̯ɡɐ"
 compIPA = html.unescape(source_escape_compIPA)
 
 ## TODO Unsecaping the HTML Stuff
-source_escape_ipa_list = [{'Word': 'zonnebloem', 'IPA': 'ˈzɔ.nə.bloːm'}, {'Word': 'zonnegeel', 'IPA': 'ˈzɔ.nə.ɣeːl'}, {'Word': 'zonnebloem', 'IPA': 'ˈzɔ.nə.bloːm'}, {'Word': 'zonneplant', 'IPA': 'ˈzɔ.nə.ˌplɑnt'}, {'Word': 'zonnebloemknop', 'IPA': 'ˈzɔ.nə.bloːm.knɔp'}]
-ipa_list = [{'Word': 'zonnebloem', 'IPA': 'ˈzɔ.nə.bloːm'}, {'Word': 'zonnegeel', 'IPA': 'ˈzɔ.nə.ɣeːl'}, {'Word': 'zonnebloem', 'IPA': 'ˈzɔ.nə.bloːm'}, {'Word': 'zonneplant', 'IPA': 'ˈzɔ.nə.ˌplɑnt'}, {'Word': 'zonnebloemknop', 'IPA': 'ˈzɔ.nə.bloːm.knɔp'}]
-
+#source_escape_ipa_list = [{'Word': 'zonnebloem', 'IPA': 'ˈzɔ.nə.bloːm'}, {'Word': 'zonnegeel', 'IPA': 'ˈzɔ.nə.ɣeːl'}, {'Word': 'zonnebloem', 'IPA': 'ˈzɔ.nə.bloːm'}, {'Word': 'zonneplant', 'IPA': 'ˈzɔ.nə.ˌplɑnt'}, {'Word': 'zonnebloemknop', 'IPA': 'ˈzɔ.nə.bloːm.knɔp'}]
+#ipa_list = [{'Word': 'zonnebloem', 'IPA': 'ˈzɔ.nə.bloːm'}, {'Word': 'zonnegeel', 'IPA': 'ˈzɔ.nə.ɣeːl'}, {'Word': 'zonnebloem', 'IPA': 'ˈzɔ.nə.bloːm'}, {'Word': 'zonneplant', 'IPA': 'ˈzɔ.nə.ˌplɑnt'}, {'Word': 'zonnebloemknop', 'IPA': 'ˈzɔ.nə.bloːm.knɔp'}]
+ipa_list = [ {'Word': 'stofzuiger', 'IPA': 'ˈstɔfˌzœyɣər'}, {'Word': 'zuigapparaat', 'IPA': 'ˈzœyɣˌʔɑpərɑt'}, {'Word': 'stofzuiger', 'IPA': 'ˈstɔfˌzœy̯ɣər'}, {'Word': 'stofzuigmachine', 'IPA': 'ˈstɔfˌzœyɣmɑˌʃinə'}, {'Word': 'vacuümreiniger', 'IPA': 'vɑˈkʏːmˌreːnəɣər'}, {'Word': 'automatische stofzuiger', 'IPA': 'ˌʔɔtɔˈmɑtɪʃə ˈstɔfˌzœyɣər'}, {'Word': 'handstofzuiger', 'IPA': 'ˈhɑntˌstɔfˌzœyɣər'}, {'Word': 'opzuiger', 'IPA': 'ˈɔpzœyɣər'}, {'Word': 'zuigborstel', 'IPA': 'ˈzœyɣˌbɔrsl̩'}, {'Word': 'stofzuigapparaat', 'IPA': 'ˈstɔfˌzœyɣˌʔɑpərɑt'}, {'Word': 'huisstofzuiger', 'IPA': 'ˈœysˌstɔfˌzœyɣər'}, {'Word': 'zuigzuiger', 'IPA': 'ˈzœyɣˌzœyɣər'}, {'Word': 'dweilzuiger', 'IPA': 'ˈdʋɛilˌzœyɣər'}, {'Word': 'zuigborsteltje', 'IPA': 'ˈzœyɣˌbɔrstəɫjə'}, {'Word': 'vloerzuiger', 'IPA': 'ˈvlurˌzœyɣər'}, {'Word': 'stofzuigketel', 'IPA': 'ˈstɔfˌzœyɣˌkətəl'}, {'Word': 'zuigset', 'IPA': 'ˈzœyɣsɛt'}, {'Word': 'zuigkop', 'IPA': 'ˈzœyɣkɔp'}, {'Word': 'stofzuigsysteem', 'IPA': 'ˈstɔfˌzœyɣsɪˌtem'}, {'Word': 'zuigmonden', 'IPA': 'ˈzœyɣmɔndən'} ]
 ipa_test = {'Word': 'zonnebloem', 'IPA': 'ˈzɔ.nə.bloːm'}
+# I added an synonym from GPT 5.1 into the output of the gpt 4.1 and i noticed that the single output of gpt 5.1 was better than the randomb 20 synonyms, which might mean we need to aösp care about how we promt it
 n = len(ipa_list)
 ipa_Scorelist = [[0] * n for _ in range(3)]
 highest_score = 0
